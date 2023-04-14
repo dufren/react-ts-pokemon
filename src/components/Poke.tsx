204ee54ede5b4a1cda1eb79015e17ccc8cd8e588
Özgür ÇOKÇEKEN <ozgur.cokceken@istinye.edu.tr>
2023-04-14 15:13:32 +0300

    deploy

--- src/components/Poke.tsx
+++ src/components/Poke.tsx
@@ -9,22 +9,39 @@ export default function Poke() {
 
   if (!name) return <div>Something went wrong.</div>;
 
-  const { data, isLoading, isFetching } = useGetPokeQuery(name);
+  const { data, isLoading, isFetching, isSuccess } = useGetPokeQuery(name);
 
-  if (isLoading || isFetching) return <LoadingPage />;
-  if (!data) return <div>Something went wrong.</div>;
+  let content;
 
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
-      <div className="flex flex-col items-center justify-center gap-5 rounded-lg border border-gray-400 bg-gray-200 p-5">
-        <img
-          src={data.sprites.other.dream_world.front_default}
-          alt="poke-avatar"
-        />
-        <h1 className="text-2xl capitalize">{data.name}</h1>
-        <h2 className="text-lg capitalize">{data.types[0].type.name}</h2>
-        <h2 className="text-lg capitalize">{data.types[1].type.name}</h2>
+  if (isLoading || isFetching) content = <LoadingPage />;
+
+  if (!data) content = <div>Something went wrong.</div>;
+
+  if (isSuccess) {
+    content = (
+      <div className="flex min-h-screen flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center gap-5 rounded-lg border border-gray-400 bg-gray-200 p-5">
+          <img
+            src={data.sprites.other.dream_world.front_default}
+            alt="poke-avatar"
+          />
+          <h1 className="text-2xl capitalize">{data.name}</h1>
+          <div className="flex flex-row gap-5">
+            {data.types.map((type) => {
+              return (
+                <h2
+                  className="text-lg font-extrabold capitalize"
+                  key={type.slot}
+                >
+                  {type.type.name}
+                </h2>
+              );
+            })}
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  return content ?? <div>Something went wrong.</div>;
 }
